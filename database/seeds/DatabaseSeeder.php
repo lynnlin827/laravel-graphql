1c0fdfa4f832e6f2536185e68cf69d4154de6647
Lynn Lin <>
2017-07-06 15:54:27 +0800

    feature: generate fake data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Factory;
+use App\User;
+use App\News;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +14,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $factory = Factory::create();
+
+        for ($i = 0; $i < 3; $i++) {
+            User::create([
+                'code' => $factory->userName,
+                'name' => $factory->name,
+                'signature' => $factory->sentence(3),
+            ]);
+        }
+
+        for ($i = 0; $i < 5; $i++) {
+            News::create([
+                'title' => $factory->sentence(6),
+                'content' => $factory->text,
+                'summary' => $factory->sentence(10),
+                'userId' => $factory->randomElement([1, 2, 3]),
+                'posted' => 1,
+                'publishedAt' => $factory->dateTime(),
+            ]);
+        }
+
+        for ($i = 0; $i < 5; $i++) {
+            News::create([
+                'title' => $factory->sentence(6),
+                'content' => $factory->text,
+                'summary' => $factory->sentence(10),
+                'userId' => $factory->randomElement([1, 2, 3]),
+            ]);
+        }
     }
 }
